294ad8cdaea710627106b90d7ca347b3a7e2f757
Glaucio Almeida <glaucio@mail.com>
2023-02-07 13:20:23 -0300

    interface add get

--- src/domain/agendas/repository/agenda_repo.go
+++ src/domain/agendas/repository/agenda_repo.go
@@ -3,6 +3,7 @@ package repository
 import "go-gin-ddd/src/domain/agendas/model"
 
 type AgendaRepository interface {
+	Get() ([]*model.Agenda, error)
 	GetBy(disponibilidade string) (*model.Agenda, error)
 	Create(agenda model.Agenda) (*model.Agenda, error)
 }
