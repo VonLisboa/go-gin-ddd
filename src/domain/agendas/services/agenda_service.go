40dedcdd8420a186d51ccd73b1297b1da956161b
Glaucio Almeida <glaucio@mail.com>
2023-02-07 14:24:53 -0300

    service updates

--- src/domain/agendas/services/agenda_service.go
+++ src/domain/agendas/services/agenda_service.go
@@ -3,13 +3,15 @@ package service
 import (
 	"errors"
 	"fmt"
+	"strconv"
 
 	"go-gin-ddd/src/domain/agendas/model"
 	"go-gin-ddd/src/domain/agendas/repository"
 )
 
 type AgendaService interface {
-	GetBy(disponibilidade string) (*model.Agenda, error)
+	Get() ([]*model.Agenda, error)
+	GetBy(disponibilidade string) ([]*model.Agenda, error)
 	Create(agenda model.Agenda) (*model.Agenda, error)
 }
 
@@ -24,18 +26,33 @@ func NewService(repository repository.AgendaRepository) AgendaService {
 	}
 }
 
-func (s *agendaService) Create(agenda model.Agenda) (*model.Agenda, error) {
+// Get implements AgendaService
+func (s *agendaService) Get() ([]*model.Agenda, error) {
+	// implement things if necessary
+	return s.repository.Get()
+}
 
+func (s *agendaService) Create(agenda model.Agenda) (*model.Agenda, error) {
 	if err := agenda.ValidateCnpj(); err != nil {
+		return nil, err
+	}
+
+	if err := agenda.ValidateHorario(); err != nil {
 		return nil, err
 	}
 
 	return s.repository.Create(agenda)
 }
 
-func (s *agendaService) GetBy(disponibilidade string) (*model.Agenda, error) {
-	if len(disponibilidade) == 0 {
-		return nil, errors.New("invalid params")
+func (s *agendaService) validateDisponibilidade(disponibilidade string) error {
+	_, err := strconv.Atoi(disponibilidade)
+	return err
+}
+
+func (s *agendaService) GetBy(disponibilidade string) ([]*model.Agenda, error) {
+
+	if err := s.validateDisponibilidade(disponibilidade); err != nil {
+		return nil, err
 	}
 
 	agenda, err := s.repository.GetBy(disponibilidade)
